9d34052ba0ee31c48ab406d2d593a7f102c2f7c5
LacanS <130593427+LACAN-Simon@users.noreply.github.com>
2023-06-02 11:15:26 +0200

    Update broadcast-example.c

--- src/broadcast-example.c
+++ src/broadcast-example.c
@@ -154,9 +154,8 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
 
     //id = nid * clock_seconds();
     id = pcg32_random_r(&rng);
-    for (i=0; i<NB_PACKETS; i++) {
-	int16_t temp ; 
-        temp = lps331ap_read_temp(&lps331ap);
+    for (i=0; i<NB_PACKETS; i++) { 
+        int16_t temp = lps331ap_read_temp(&lps331ap);
 	snprintf(send_buffer, sizeof(uint32_t)*8, "ID:%lx; T=%i", id+i,temp);
     	printf("Sending broadcast;%s\n", send_buffer);
     	uip_create_linklocal_allnodes_mcast(&addr);
