8606e113ebc0114fd9f1781c1e8be7db0363180b
LacanS <130593427+LACAN-Simon@users.noreply.github.com>
2023-06-06 09:40:31 +0200

    Update broadcast-example.c

--- src/broadcast-example.c
+++ src/broadcast-example.c
@@ -187,11 +187,10 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
 	float l = process_light();
 	float p = process_pressure();
 	snprintf(send_buffer, sizeof(uint32_t)*30, "%.2f;%.2f;%lx", l,p,id+i);
-	//printf("Sending:%s\n", send_buffer);
-	/*size_t send_buffer_siz = strlen(send_buffer);
-	printf("Taille de send_buffer : %lu\n", (unsigned long)send_buffer_siz);*/
+
     	uip_create_linklocal_allnodes_mcast(&addr);
-    	//simple_udp_sendto(&broadcast_connection, send_buffer, sizeof(send_buffer), &addr);
+	size_t send_buffer_siz = strlen(send_buffer);
+	printf("Taille de send_buffer : %lu\n", (unsigned long)send_buffer_siz);
 	simple_udp_sendto(&broadcast_connection, send_buffer,sizeof(send_buffer), &addr);
 	printf("Sending:%s\n", send_buffer);
 	size_t send_buffer_size = strlen(send_buffer);
