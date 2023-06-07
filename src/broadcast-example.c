b76ee9c9d2da34228fcd3528fe588b7fa7c907a4
LacanS <130593427+LACAN-Simon@users.noreply.github.com>
2023-06-07 16:21:20 +0200

    Update broadcast-example.c

--- src/broadcast-example.c
+++ src/broadcast-example.c
@@ -259,10 +259,10 @@ PROCESS_THREAD(broadcast_example_process, ev, data)
 		simple_udp_sendto(&broadcast_connection,send_buffer,sizeof(send_buffer), &addr) ;
 		tab[2]=t;
 	}
-    int k = 0;
+    /*int k = 0;
     for (k = 0; k < 3; i++) {
         printf("%f ", tab[k]);
-    }
+    }*/
     } 
 
   }
