773d9addbdc51ad6e614149b0065738e4cbfb553
LacanS <130593427+LACAN-Simon@users.noreply.github.com>
2023-06-09 09:30:07 +0200

    Update exp.sh

--- scripts/exp.sh
+++ scripts/exp.sh
@@ -5,7 +5,7 @@ set -e
 
 #---------------------- TEST ARGUMENTS ----------------------#
 if [ "$#" -ne 6 ]; then
-	echo "Usage: $0 <exp name> <interval (s)> <payload size (B)> <exp duration (m)> <packets per seconds> <list of nodes>"
+	echo "Usage: $0 <exp name> <interval (s)> <exp duration (m)> <packets per seconds> <list of nodes>"
 	exit
 fi  
 #---------------------- TEST ARGUMENTS ----------------------#
@@ -13,6 +13,8 @@ fi
 #--------------------- DEFINE VARIABLES ---------------------#
 LOGIN="lacan"
 SITE="lille"
+LU = ",m3,"
+L = "$SITE$LU$5"
 IOTLAB="$LOGIN@$SITE.iot-lab.info"
 CODEDIR="${HOME}/path/to/iot-lab/parts/contiki/examples/ipv6/simple-udp-rpl"
 EXPDIR="${HOME}/stage"
@@ -31,7 +33,6 @@ function ctrl_c() {
 #-------------------- CONFIGURE FIRMWARE --------------------#
 sed -i "s/#define\ SEND_INTERVAL_SECONDS\ .*/#define\ SEND_INTERVAL_SECONDS\ $2/g" $CODEDIR/broadcast-example.c
 sed -i "s/#define\ SEND_BUFFER_SIZE\ .*/#define\ SEND_BUFFER_SIZE\ $3/g" $CODEDIR/broadcast-example.c
-sed -i "s/#define\ NB_PACKETS\ .*/#define\ NB_PACKETS\ $5/g" $CODEDIR/broadcast-example.c
 #-------------------- CONFIGURE FIRMWARE --------------------#
 
 #--------------------- COMPILE FIRMWARE ---------------------#
@@ -43,7 +44,7 @@ make TARGET=iotlab-m3 -j8 || { echo "Compilation failed."; exit 1; }
 cd $EXPDIR/scripts
 
 # Launch the experiment and obtain its ID
-EXPID=$(iotlab-experiment submit -n $1 -d $4 -l $6 | grep id | cut -d' ' -f6)
+EXPID=$(iotlab-experiment submit -n $1 -d $4 -l $L | grep id | cut -d' ' -f6)
 # Wait for the experiment to began
 iotlab-experiment wait -i $EXPID
 # Flash nodes
