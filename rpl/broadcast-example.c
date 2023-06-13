603bc76e65aa61b44566c2f5594407e7e73c382f
LacanS <130593427+LACAN-Simon@users.noreply.github.com>
2023-06-13 15:22:11 +0200

    Update broadcast-example.c

--- rpl/broadcast-example.c
+++ rpl/broadcast-example.c
@@ -15,18 +15,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
+#define UDP_PORT 1234
+#define SEND_BUFFER_SIZE 1000
+#define BEGIN_INTERVAL_SECONDS 10 
+#define BEGIN_INTERVAL  (BEGIN_INTERVAL_SECONDS * CLOCK_SECOND)
+#define NB_PACKETS 1
+#define SEND_INTERVAL_SECONDS 6
+#define SEND_INTERVAL		(SEND_INTERVAL_SECONDS * CLOCK_SECOND)
+#define SEND_TIME		(random_rand() % (SEND_INTERVAL))
+
+/*---------------------------------------------------------------------------*/
+PROCESS(broadcast_example_process, "UDP broadcast example process");
+AUTOSTART_PROCESSES(&broadcast_example_process);
+/*---------------------------------------------------------------------------*/
 
 //
 /*---------------------------------------------------------------------------*/
-PROCESS(rpl_creation, "create an rpl network");
-AUTOSTART_PROCESSES(&rpl_creation);
-PROCESS_THREAD(rpl_creation,ev,data)
+PROCESS_THREAD(broadcast_example_process, ev, data)
 {
-  printf("création du rpl");
+
   rpl_init(); 
   uip_ipaddr_t ipaddr;
-	
-  PROCESS_BEGIN();	
   uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0, 0, 0, 0);
   uip_ds6_set_addr_iid(&ipaddr, &uip_lladdr);
   uip_ds6_addr_add(&ipaddr, 0, ADDR_AUTOCONF);
@@ -35,22 +44,17 @@ PROCESS_THREAD(rpl_creation,ev,data)
   dag = rpl_set_root(RPL_DEFAULT_INSTANCE, &ipaddr);
   rpl_set_prefix(dag, &ipaddr, 64);
   rpl_set_mode(RPL_MODE_MESH);
-  while(1){
-	  if(dag != NULL) {
-		  printf("DAG created\n");   }
-	//   rpl_dag_t *dag = rpl_get_any_dag();
-	  if(dag != NULL && dag->preferred_parent != NULL) {
-	  printf("Preferred parent IP address: %u\n", dag->preferred_parent->flags);
-	} else {
-	  printf("No preferred parent\n");
-	}
-	// rpl_dag_t *dag = rpl_get_any_dag();
-	if(dag != NULL) {
-	  printf("Node rank: %u\n", dag->rank);
-	} else {
-	  printf("DAG not available\n");
-	}
+  if(dag != NULL) {
+  printf("DAG created\n");
+} else {
+  printf("DAG creation failed\n");
+}
+  PROCESS_BEGIN();
+  
+  while(1) {
+ 	printf("la zone est quadrillée");
   }
+	
   PROCESS_END();
 }
 /*---------------------------------------------------------------------------*/
